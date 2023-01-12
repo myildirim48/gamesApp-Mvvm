aeacab93da475c25804b6f81c05c871088aa6fed
Muhammed YILDIRIM <muhammedyildirim48@gmail.com>
2023-01-12 21:32:30 +0300

    Initial Commit

---
+++ myGames/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myGames
+//
+//  Created by YILDIRIM on 12.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
